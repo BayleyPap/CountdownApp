2f1c37332812c0ae7cbae001da034b98294981c5
bayley <bayley.papworth@charterkc.com.au>
2017-11-13 17:16:02 +1100

    improved styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component} from 'react';
 import Clock from './Clock'
 import './App.css';
+import {Form, FormControl, Button} from 'react-bootstrap';
+
 
 class App extends Component{
   constructor(props){
@@ -25,10 +27,10 @@ class App extends Component{
             deadline={this.state.deadline}
           />
         </div>
-        <div>
-          <input placeholder='new date' onChange={event => this.setState({newDeadline: event.target.value})} />
-          <button onClick={()=> this.changeDeadline()}>Submit</button>
-        </div>
+        <Form inline>
+          <FormControl className="Deadline-input" placeholder='new date' onChange={event => this.setState({newDeadline: event.target.value})} />
+          <Button onClick={()=> this.changeDeadline()}>Submit</Button>
+        </Form>
       </div>
     )
 
